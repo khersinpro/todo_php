8a32287db54ec66a4d342907205aae9bda40900a
khersinpro <khersinpro@gmail.com>
2022-10-20 13:32:36 +0200

    first commuit

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <?php include_once './includes/head.php' ?>
+    <title>Todo PHP</title>
+</head>
+<body>
+    <div class="container">
+        <?php require_once './includes/header.php' ?>
+        <div class="content">
+            <div class="todo-container">
+                <h1>Ma Todo</h1>
+                <div class="todo-form">
+
+                </div>
+                <div class="todo-list">
+
+                </div>
+            </div>
+        </div>
+        <?php include_once './includes/footer.php' ?>
+    </div>
+</body>
+</html>
